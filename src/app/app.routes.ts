fbd9393e21758794e35e402579b087c8e9559b18
AvigailSalomons <148711168+AvigailSalomons@users.noreply.github.com>
2024-03-31 14:39:19 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-    { path: "", redirectTo: "", pathMatch: "full" },
+    { path: "", redirectTo: "recipe", pathMatch: "full" },
     {path:'recipe',loadChildren:()=>import('./recipe/recipe.module').then(c=>c.RecipeModule)},
     {path:'user',loadChildren:()=>import('./user/user.module').then(c=>c.UserModule)}
 
